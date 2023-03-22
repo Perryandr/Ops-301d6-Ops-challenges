23885ab270bb33d51a7ca0b758e655e8c1887abf
Andrew Perry <81926324+Perryandr@users.noreply.github.com>
2023-03-22 09:09:07 -0700

    Update Ops301challenge7.py

--- Ops301challenge7.py
+++ Ops301challenge7.py
@@ -5,3 +5,15 @@
 # Date of lastest revision: 03/21/2023
 # Purpose: Code Fellows requires it to pass class.
 
+import os
+
+def generate_directory_structure(user_path):
+    for dirpath, dirnames, filenames in os.walk(user_path):
+        print(f"Directory Path: {dirpath}")
+        print(f"Directories: {dirnames}")
+        print(f"Files: {filenames}\n")
+
+user_input = input("Enter a directory path: ")
+generate_directory_structure(user_input)
+
+
