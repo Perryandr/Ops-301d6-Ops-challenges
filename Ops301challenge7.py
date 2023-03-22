3f55c7a6492c0d59c516bfb0a6c0d70359f5b9ab
Andrew Perry <81926324+Perryandr@users.noreply.github.com>
2023-03-21 21:35:47 -0700

    Create Ops301challenge7.py

---
+++ Ops301challenge7.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Script: Ops 301 Class 06 Ops Challenge Solution
+# Author: Andrew P.
+# Date of lastest revision: 03/21/2023
+# Purpose: Code Fellows requires it to pass class.
+
