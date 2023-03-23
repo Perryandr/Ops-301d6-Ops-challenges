255b2cf12f79692fc6485cf583eb9b1bbb5b1ed0
Andrew Perry <81926324+Perryandr@users.noreply.github.com>
2023-03-22 22:18:30 -0700

    Create Ops301challenge8.py

---
+++ Ops301challenge8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Script: Ops 301 Class 08 Ops Challenge Solution
+# Author: Andrew P.
+# Date of lastest revision: 03/22/2023
+# Purpose: Code Fellows requires it to pass class.
+
+
+
+
+
+
+
+# Assign a list of ten string elements to a variable
+fruitsofthespirit = ['love', 'joy', 'peace', 'longsuffering', 'gentleness', 'goodness', 'faith', 'meekness', 'temperance', 'compassion']
+
+# Print the fourth element of the list
+print(fruitsofthespirit[3])
+
+# Print the sixth through tenth element of the list
+print(fruitsofthespirit[5:])
+# Alternatively, you can use the following syntax to specify the range:
+# print(my_list[5:10])
+
+# Change the value of the seventh element to "onion"
+fruitsofthespirit[6] = 'onion'
+print(fruitsofthespirit)
