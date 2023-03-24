29f30d40cb248e2119a5de9c87e1668f05d83068
Andrew Perry <81926324+Perryandr@users.noreply.github.com>
2023-03-24 00:26:38 -0700

    Create Ops301challenge9.py

---
+++ Ops301challenge9.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Script: Ops 301 Class 09 Ops Challenge Solution
+# Author: Andrew P.
+# Date of lastest revision: 03/24/2023
+# Purpose: Code Fellows requires it to pass class.
+
+
+
+
+# Create an if statement using using logical conditions.
+a = 7
+b = 7
+
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
+
+
+# Create an if statement that has elif and else.
+a = 15
+b = 20
+
+if a >= b:
+    print("a is greater than or equal to b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("This statement should not be reached")
