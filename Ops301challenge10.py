b9cb42693fd239a8a8158c3768cede080f00a9e2
Andrew Perry <81926324+Perryandr@users.noreply.github.com>
2023-03-24 21:57:03 -0700

    Update Ops301challenge10.py

--- Ops301challenge10.py
+++ Ops301challenge10.py
@@ -5,6 +5,35 @@
 # Date of lastest revision: 03/25/2023
 # Purpose: Code Fellows requires it to pass class.
 
+# Credit for resources and reference goes to Code academy and itsolutionstuff. 
+# https://www.codecademy.com/learn/learn-python-3/modules/learn-python3-files/cheatsheet
+# https://www.itsolutionstuff.com/post/how-to-create-multiline-text-file-in-pythonexample.html
+
+
+# Create a script that creates a new .txt file.
+
+# Appends three lines,
+
+# prints to screen the first line
+
+
+# deletes the file
+
+# Code together
+import os
 
 
 
+with open("test.txt", "w") as f:
+    line1 = "Every master started their journey by taking the first step. \n"
+    line2= "Let us start our journey today. \n"
+    line3 = "What direction will we go?"
+
+    f.writelines([line1, line2, line3])
+
+    print(line1)
+    
+ 
+os.remove(journey.txt")    
+    
+    
