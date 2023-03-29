183f7f83a645030d7e87e3ecd8994b70f7662ce9
Andrew Perry <81926324+Perryandr@users.noreply.github.com>
2023-03-28 20:11:20 -0700

    Update Ops301challenge11.py

--- Ops301challenge11.py
+++ Ops301challenge11.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-# Script: Ops 301 Class 10 Ops Challenge Solution
+# Script: Ops 301 Class 11 Ops Challenge Solution
 # Author: Andrew P.
-# Date of lastest revision: 03/25/2023
+# Date of lastest revision: 03/27/2023
 # Purpose: Code Fellows requires it to pass class.
 
 
